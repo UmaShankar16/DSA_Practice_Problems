5b1a4b2afadc3aad05cee9d335e02ec47da3a90d
UmaShankar16 <singhumashankar1607@gmail.com>
2026-03-25 13:25:50 +0530

    It checks parentheses is valid or not

---
+++ Stack/validParentheses.java
@@ -0,0 +1,39 @@
+package Stack;
+
+import java.util.Stack;
+
+class validParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{')
+            {
+                stack.push(ch);
+            }
+            else
+            {
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                else if(ch==')'&&stack.peek()=='(')
+                    stack.pop();
+                else if(ch==']'&&stack.peek()=='[')
+                    stack.pop();
+                else if(ch=='}'&&stack.peek()=='{')
+                    stack.pop();
+                else 
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s="([{}])";
+        System.out.println(" "+isValid(s));
+    }
+}
