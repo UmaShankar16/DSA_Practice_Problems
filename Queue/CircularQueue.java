110bc0a805afbde68cf71d06ef4cb02bd8bf80bd
UmaShankar16 <singhumashankar1607@gmail.com>
2026-03-31 13:11:03 +0530

    Circular Queue Implementation using Array

---
+++ Queue/CircularQueue.java
@@ -0,0 +1,83 @@
+/*Circular Queue implementation using Array. */
+/*If you want to implement queue using fixed array. Then it is recommended to use circular queue 
+bcs enqueue,dequeue and peek operations time complexity will be O(1). */
+public class CircularQueue
+{
+    static int rear=-1;
+    static int front=-1;
+    static int arr[];
+    static int size;
+    CircularQueue(int n)
+    {
+        arr=new int[n];
+        this.size=n;
+    }
+    public static boolean isEmpty()
+    {
+        return rear==-1 && front==-1;
+    }
+    public static boolean isFull()
+    {
+        return (rear+1)%size==front;
+    }
+    public static void add(int data)
+    {
+        if(isFull())
+        {
+            System.err.println("Queue is full");
+            return;
+        }
+        if(front==-1)
+        {
+            front=0;
+        }
+        rear=(rear+1)%size;
+        arr[rear]=data;
+    }
+    public static int remove()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int result=arr[front];
+        if(rear==front)
+        {
+            rear=front=-1;
+        }
+        else
+        {
+            front=(front+1)%size;
+        }
+        return result;
+    }
+    public static int peek()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return arr[front];
+    }
+    public static void main(String[] args)
+    {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q.remove());
+        q.add(6);
+        System.out.println(q.remove());
+        q.add(7);
+        System.out.println(q.remove());
+        while(!q.isEmpty())
+        {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
